bce995d148367f812522495b6e545eef230df6c4
pabloac31 <pabloac31@hotmail.com>
2020-03-31 18:01:19 +0200

    changes

--- adversarial_methods.py
+++ adversarial_methods.py
@@ -8,6 +8,6 @@ def fgsm(image, epsilon, data_grad):
   # Create the perturbed image by adjusting each pixel of the input image
   perturbed_image = image + epsilon*sign_data_grad
   # Adding clipping to maintain [0,1] range
-  perturbed_image = torch.clamp(perturbed_image, 0, 1)
+  perturbed_image = clamp_norm(perturbed_image, 0, 1)
   # Return the perturbed image
   return perturbed_image
--- utils_cifar10.py
+++ utils_cifar10.py
@@ -11,33 +11,61 @@ import matplotlib.pyplot as plt
 
 from adversarial_methods import *
 
-def cifar10_dataloader(path):
+def testloader(path, batch_size):
   transform = transforms.Compose([transforms.ToTensor(),
       transforms.Normalize([0.4914, 0.4822, 0.4465], [0.2023, 0.1994, 0.2010])])
 
   test_loader = torch.utils.data.DataLoader(
       datasets.CIFAR10(root=path, train=False, transform=transform, download=True),
-      batch_size=1, shuffle=False
+      batch_size=batch_size, shuffle=True
   )
 
   return test_loader
 
-def test_model(model, device, test_loader, epsilon):
 
+def test_model(model, device, test_loader):
+    
+    model = model.to(device).eval()     
+    logs = {'Accuracy': 0.0}
+            
+    # Iterate over data
+    for image, label in pbar(test_loader):
+        image = image.to(device)
+        label = label.to(device)
+
+        with torch.no_grad():
+            prediction = model(image)
+            accuracy = torch.sum(torch.max(prediction, 1)[1] == label.data).item()
+            logs['Accuracy'] += accuracy
+
+    logs['Accuracy'] /= len(test_loader.dataset)
+    
+    return logs['Accuracy']
+
+
+def test_fgsm(model, device, test_loader, epsilon, iters=10000):
+  
   # Initialize the network and set the model in evaluation mode. 
   model = model.to(device).eval()
 
   # Accuracy counter
   correct = 0
-  misclassified = 0  # without the adversarial attack
+  confidence = 0
+  total_time = 0
+  robustness = 0
   adv_examples = []
 
+  i = 0
+
   # Loop all examples in test set
-  for data, target in test_loader:
+  for data, target in pbar(test_loader):
+    if i >= iters:
+      break
+    i += 1
 
     # Send the data and label to the device
     data, target = data.to(device), target.to(device)
-
+  
     # Set requires_grad attribute of tensor. Important for Attack
     data.requires_grad = True
 
@@ -47,7 +75,6 @@ def test_model(model, device, test_loader, epsilon):
 
     # If the initial prediction is wrong, dont botter attacking
     if init_pred.item() != target.item():
-      misclassified += 1
       continue
 
     # Calculate the loss
@@ -63,29 +90,37 @@ def test_model(model, device, test_loader, epsilon):
     data_grad = data.grad.data
 
     # Call FGSM attack
+    time_ini = time.time()
     perturbed_data = fgsm(data, epsilon, data_grad)
+    time_end = time.time()
+    total_time += time_end-time_ini
+
+    # Update model robustness
+    delta = math.sqrt(32*32*3*epsilon**2)  # fgsm has a fixed delta(f,x) robustness
+    im_np = data.squeeze().detach().cpu().numpy()
+    robustness += delta / np.linalg.norm(im_np.flatten(), ord=2)
 
     # Re-classify the perturbed image
     output = model(perturbed_data)
 
     # Check for success
     final_pred = output.max(1, keepdim=True)[1] # get the index of the max log-probability
+    confidence += F.softmax(output, dim=1).max(1, keepdim=True)[0].item()  # adv. confidence
     if final_pred.item() == target.item():
       correct += 1
-      # Special case for saving 0 epsilon examples
-      if (epsilon == 0) and (len(adv_examples) < 5):
-        adv_ex = perturbed_data.squeeze().detach().cpu().numpy()
-        adv_examples.append( (init_pred.item(), final_pred.item(), adv_ex) )
-
     else:
       # Save some adv examples for visualization later
       if len(adv_examples) < 5:
         adv_ex = perturbed_data.squeeze().detach().cpu().numpy()
-        adv_examples.append( (init_pred.item(), final_pred.item(), adv_ex) )
+        adv_examples.append( (init_pred.item(), final_pred.item(), im_np, adv_ex) )
 
   # Calculate final accuracy for this epsilon
-  final_acc = correct / float(len(test_loader))
-  print("Epsilon: {}\tTest Accuracy = {} / {} = {}".format(epsilon, correct, len(test_loader), final_acc))
+  final_acc = correct / float(iters)  # len(test_loader)
+  avg_confidence = confidence / float(iters)
+  avg_time = total_time / float(iters)
+  model_robustness = robustness / float(iters)
+  print("Epsilon: {}\nTest Accuracy = {} / {} = {}\nAverage confidence = {}\nAverage time = {}\nAverage magnitude of perturbations = {}\nModel robustness = {}"
+    .format(epsilon, correct, iters, final_acc, avg_confidence, avg_time, delta, model_robustness))  # len(test_loader)
 
   # Return the accuracy and adversarial examples
   return final_acc, adv_examples
@@ -106,3 +141,18 @@ def plot_examples(epsilons, examples):
       plt.imshow(ex, cmap="gray")
   plt.tight_layout()
   plt.show()
+
+
+def clamp_norm(img, inf, sup):
+  mean, std = [0.4914, 0.4822, 0.4465], [0.2023, 0.1994, 0.2010]
+  im = img.detach().cpu().numpy()
+  for channel in range(3):
+    lim_inf = (inf-mean[channel]) / std[channel]
+    lim_sup = (sup-mean[channel]) / std[channel]
+    for i, arr in enumerate(im[0][channel]):
+      for j, pixel in enumerate(arr):
+        if pixel < lim_inf:
+          im[0][channel][i][j] = lim_inf
+        elif pixel > lim_sup:
+          im[0][channel][i][j] = lim_sup
+  return (torch.from_numpy(im).to(device))
