c0b5b9a5ca71e30758525c6f85e6d994c8cd004a
pabloac31 <pabloac31@hotmail.com>
2020-04-01 13:23:40 +0200

    imports

--- utils_cifar10.py
+++ utils_cifar10.py
@@ -8,6 +8,7 @@ import torch.optim as optim
 from torchvision import datasets, transforms
 import numpy as np
 import matplotlib.pyplot as plt
+from tqdm import tqdm as pbar
 
 from adversarial_methods import *
 
@@ -56,89 +57,6 @@ def test_model(model, device, test_loader):
     logs['Accuracy'] /= len(test_loader.dataset)
     
     return logs['Accuracy']
-
-
-def test_fgsm(model, device, test_loader, epsilon, iters=10000):
-  
-  # Initialize the network and set the model in evaluation mode. 
-  model = model.to(device).eval()
-
-  # Accuracy counter
-  correct = 0
-  confidence = 0
-  total_time = 0
-  robustness = 0
-  adv_examples = []
-
-  i = 0
-
-  # Loop all examples in test set
-  for data, target in pbar(test_loader):
-    if i >= iters:
-      break
-    i += 1
-
-    # Send the data and label to the device
-    data, target = data.to(device), target.to(device)
-  
-    # Set requires_grad attribute of tensor. Important for Attack
-    data.requires_grad = True
-
-    # Forward pass the data through the model
-    output = model(data)
-    init_pred = output.max(1, keepdim=True)[1] # get the index of the max log-probability
-
-    # If the initial prediction is wrong, dont botter attacking
-    if init_pred.item() != target.item():
-      continue
-
-    # Calculate the loss
-    loss = F.nll_loss(output, target)
-
-    # Zero all existing gradients
-    model.zero_grad()
-
-    # Calculate gradients of model in backward pass
-    loss.backward()
-
-    # Collect datagrad
-    data_grad = data.grad.data
-
-    # Call FGSM attack
-    time_ini = time.time()
-    perturbed_data = fgsm(data, epsilon, data_grad)
-    time_end = time.time()
-    total_time += time_end-time_ini
-
-    # Update model robustness
-    delta = math.sqrt(32*32*3*epsilon**2)  # fgsm has a fixed delta(f,x) robustness
-    im_np = data.squeeze().detach().cpu().numpy()
-    robustness += delta / np.linalg.norm(im_np.flatten(), ord=2)
-
-    # Re-classify the perturbed image
-    output = model(perturbed_data)
-
-    # Check for success
-    final_pred = output.max(1, keepdim=True)[1] # get the index of the max log-probability
-    confidence += F.softmax(output, dim=1).max(1, keepdim=True)[0].item()  # adv. confidence
-    if final_pred.item() == target.item():
-      correct += 1
-    else:
-      # Save some adv examples for visualization later
-      if len(adv_examples) < 5:
-        adv_ex = perturbed_data.squeeze().detach().cpu().numpy()
-        adv_examples.append( (init_pred.item(), final_pred.item(), im_np, adv_ex) )
-
-  # Calculate final accuracy for this epsilon
-  final_acc = correct / float(iters)  # len(test_loader)
-  avg_confidence = confidence / float(iters)
-  avg_time = total_time / float(iters)
-  model_robustness = robustness / float(iters)
-  print("Epsilon: {}\nTest Accuracy = {} / {} = {}\nAverage confidence = {}\nAverage time = {}\nAverage magnitude of perturbations = {}\nModel robustness = {}"
-    .format(epsilon, correct, iters, final_acc, avg_confidence, avg_time, delta, model_robustness))  # len(test_loader)
-
-  # Return the accuracy and adversarial examples
-  return final_acc, adv_examples
 
 
 def plot_examples(epsilons, examples):
