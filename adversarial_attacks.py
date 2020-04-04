696561a1a01c6581d12ebeab04ec80d69b517d4f
pabloac31 <pabloac31@hotmail.com>
2020-04-04 18:18:28 +0200

    deepfool

--- adversarial_attacks.py
+++ adversarial_attacks.py
@@ -90,39 +90,51 @@ def fgsm(model, image, epsilon, output, label):
 
 
 def deepfool(model, device, image, label, output, num_classes=10, overshoot=0.02, max_iter=50):
-  # Classes ordered by probability (descending)
-  f_image = output.data.cpu().numpy().flatten()
-  I = f_image.argsort()[::-1]
-  I = I[0:num_classes]  # pick only num_classes
 
-  # Start from original image (copy)
+  # Array with the class probabilities of the image
+  f_image = output.data.cpu().numpy().flatten()
+  # Classes ordered by probability (descending)
+  I = f_image.argsort()[::-1]
+  # We consider only 'num_classes' classes
+  I = I[0:num_classes]
+
+  # Get the input image shape
   input_shape = image.cpu().detach().numpy()[0].shape   # (3,H,W)
+  # Start from a copy of the original image
   pert_image = copy.deepcopy(image)   # tensor of size (1,3,H,W)
+
+  # Initialize variables
   w = np.zeros(input_shape)
   r_tot = np.zeros(input_shape)
-
   loop_i = 0
 
+  # x_0 <- x
   x = pert_image.clone().detach().requires_grad_(True)
+  # output for x_0
   fs = model(x)
+  # original label
   k_i = label
 
   while k_i == label and loop_i < max_iter:
 
     pert = np.inf
+
+    # Calculate grad(f_label(x_i))
     fs[0, I[0]].backward(retain_graph=True)
     grad_orig = x.grad.data.cpu().numpy().copy()
 
-    for k in range(1, num_classes):
+    for k in range(1, num_classes):  # for k != label
       zero_gradients(x)
 
+      # Calculate grad(f_k(x_i))
       fs[0, I[k]].backward(retain_graph=True)
       cur_grad = x.grad.data.cpu().numpy().copy()
 
-      # set new w_k and new f_k
+      # Set new w_k and new f_k
       w_k = cur_grad - grad_orig
       f_k = (fs[0, I[k]] - fs[0, I[0]]).data.cpu().numpy()
 
+      # Calculate hyperplane-k distance
       pert_k = abs(f_k)/np.linalg.norm(w_k.flatten())
 
       # determine which w_k to use
@@ -135,18 +147,25 @@ def deepfool(model, device, image, label, output, num_classes=10, overshoot=0.02
     r_i = (pert+1e-4) * w / np.linalg.norm(w)
     r_tot = np.float32(r_tot + r_i)
 
+    # Update adversarial image
     if device == torch.device("cuda"):
       pert_image = image + (1+overshoot)*torch.from_numpy(r_tot).cuda()
     else:
       pert_image = image + (1+overshoot)*torch.from_numpy(r_tot)
 
+    # x_(i+1) <- x_i + r_i
     x = pert_image.clone().detach().requires_grad_(True)
+    # output for x_(i+1)
     fs = model(x)
+    # label assigned to x_(i+1)
     k_i = np.argmax(fs.data.cpu().numpy().flatten())
 
     loop_i += 1
 
   r_tot = (1+overshoot)*r_tot
+
+  # Adding clipping to maintain [0,1] range !!!!
+  pert_image = clamp_cifar10(pert_image, 0, 1)
 
   return pert_image
 
